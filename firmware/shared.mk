7fea6ac54c112543646d5fdf8e1aa153d82681ec
Martin Povišer <povik@protonmail.com>
2023-05-25 10:40:10 +0200

    firmware: Consolidate make rules
    Signed-off-by: Martin Povišer <povik@cutebit.org>

--- firmware/shared.mk
+++ firmware/shared.mk
@@ -4,12 +4,10 @@ LEAPTOOLS_IMG = PYTHONPATH=../../:$PYTHONPATH python3 -m leaptools.image
 $(FWNAME).bin: topology.conf leap_firmware.leapfrog
 	ALSA_CONFIG_TPLG=. alsatplg -c $< -o $@
 
-leap_firmware_pre.leapfrog: leap_firmware.py
-	$(LEAPTOOLS) -p 'compile_dsl "leap_firmware.py"; image_write "leap_firmware_pre.leapfrog"'
-
-leap_firmware.leapfrog: leap_firmware_pre.leapfrog Makefile
+leap_firmware.leapfrog: leap_firmware.py
+	$(LEAPTOOLS) -p 'compile_dsl "leap_firmware.py"; image_write "$@.tmp"'
 	echo "000000: c900 0000 c900 0000 0000 0000 c900 0000" | \
-			xxd -r | $(LEAPTOOLS_IMG) -s $@.tmp $< -a 0x30101 -l 6
+			xxd -r | $(LEAPTOOLS_IMG) -s $@.tmp $@.tmp -a 0x30101 -l 6
 	$(LEAPTOOLS_IMG) -s $@.tmp $@.tmp --imprint "$(FWNAME) `git describe --tags --always --dirty`"
 	cp $@.tmp $@
 	rm $@.tmp
